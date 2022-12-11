d6ac774606e47b524d682c03f49ae44f50a03158
Matthew C <82059786+RedBean3ll@users.noreply.github.com>
2022-12-11 00:03:22 -0500

    Source Code and Zipped Copy
    Code and files of HTML clock

---
+++ MK WII Themed Clock/index_files/digitalClockImage.js
@@ -0,0 +1,108 @@
+﻿// -----------------------------------------------------------------
+// Digital Clock vol.1 デジタル時計
+// CopyRight(C) 2014 Simple Style, All rights reserved.
+// Modified by Matthew C
+// Project completion date: 10/19/2022
+//
+// Modifications:
+// -day_of_week +12hr_toggle +AMPM_toggle, 
+// +second_toggle +blink_toggle
+//
+// Note: Cleaned up existing functions
+// -----------------------------------------------------------------
+
+//12-hr mode, Ante Meridiem, Seconds, Blinks
+let rules = [true, true, true, true]; 
+let blinkInt = 500;
+
+( 
+function(){ //IIFE start
+// Output of the elements.
+document.addEventListener('DOMContentLoaded',outputClock,false);
+function outputClock(){
+  //Check Rules
+  if(!rules[1]) { hideAnteMeridiem(); }
+  if(!rules[2]) { hideSecconds(); }
+}
+
+function hideSecconds() {
+  const SECONDA = document.getElementById("Nums6");
+  const SECCNDB = document.getElementById("Nums7");
+  const COLONB = document.getElementById("Nums5");
+  SECONDA.style.display = "none";
+  SECCNDB.style.display = "none";
+  COLONB.style.display = "none";
+}
+
+function hideAnteMeridiem() {
+  const ANTMERD = document.getElementById("Nums8");
+  ANTMERD.style.display = "none";
+}
+
+// Move the numbers. (NUMBERS SPLIT INTO SINGLE DIGITS)
+function moveDigital(){
+  
+  var now   = new Date();
+  var hour = now.getHours();
+
+  var antemeridiem = (hour > 11) ? "hr_pm.png" : "hr_am.png";
+
+  //HOURS
+  var hour2;
+  var hour1;
+  if(rules[0]) {
+    hour = hoursTo12(now.getHours);
+
+    if(hour > 9) {
+      hour1 = 1;
+      hour2 = hour - 10;
+    } else {
+      hour1 = 0;
+      hour2 = hour;
+    }
+  } else {
+    hour2 = hour % 10;
+    hour1 = (hour - hour2) / 10;
+  }
+
+  //MINUETS
+  var mine   = now.getMinutes();
+  var mine2  = mine % 10;
+  var mine1  = (mine - mine2) / 10; 
+
+  //SECONDS
+  var sec    = now.getSeconds();
+  var sec2   = sec % 10;
+  var sec1   = (sec - sec2) / 10;
+
+  document.getElementById('dig0').src = 'index_files/'+hour1+'.png';
+  document.getElementById('dig1').src = 'index_files/'+hour2+'.png';
+  document.getElementById('dig3').src = 'index_files/'+mine1+'.png';
+  document.getElementById('dig4').src = 'index_files/'+mine2+'.png';
+  document.getElementById('dig6').src = 'index_files/'+sec1+'.png';
+  document.getElementById('dig7').src = 'index_files/'+sec2+'.png';
+  document.getElementById('dig8').src = 'index_files/'+antemeridiem;
+}
+
+//24hr to 12hr
+function hoursTo12(hour) { return hour % 12 || 12; }
+
+//Blink effect
+function blinks(){
+  const COL_OBJ = document.getElementById('dig5').style;
+  const COL2_OBJ = document.getElementById('dig2').style;
+
+  if ( COL_OBJ.visibility=='hidden') {
+    COL_OBJ.visibility = 'visible';
+    COL2_OBJ.visibility = 'visible';
+  }       
+  else {
+    COL_OBJ.visibility = 'hidden';
+    COL2_OBJ.visibility = 'hidden';  
+  }         
+}
+
+//Intervals
+setInterval( moveDigital, 500 );
+if(rules[3]) { setInterval( blinks,blinkInt ); }
+}() ); //IIFE end
---
+++ MK WII Themed Clock/index_files/unused/digitalClockImage - originalcopy.js
@@ -0,0 +1,95 @@
+﻿// -----------------------------------------------------------------
+// Digital Clock vol.1 デジタル時計
+// CopyRight(C) 2014 Simple Style, All rights reserved.
+// -----------------------------------------------------------------
+( function(){ //IIFE start
+// Output of the elements.
+//outputClock();
+document.addEventListener('DOMContentLoaded',outputClock,false);
+function outputClock(){
+  var img_src;
+  const DIG_OBJ = document.getElementById('digits');
+  for( var i=7; i<8; i++ ){
+    const ADD_DIV = document.createElement('span');
+    ADD_DIV.setAttribute('id','Nums'+i);
+    DIG_OBJ.appendChild(ADD_DIV);
+    if( i==2 || i==5 ){ img_src = 'index_files/colon.png'; }
+    else{ img_src = 'index_files/time.png'; }
+    ADD_DIV.innerHTML = '<img id="dig'+i+'" src="'+img_src+'">';
+  }
+  moveDigital();
+}
+
+// Move the numbers.
+function moveDigital(){
+  var now   = new Date();
+  var hour   = now.getHours();
+  var hour2  = hour % 10;
+  var hour1  = (hour-hour2) / 10 ;
+  var mine   = now.getMinutes();
+  var mine2  = mine % 10;
+  var mine1  = (mine - mine2) / 10; 
+  var sec    = now.getSeconds();
+  var sec2   = sec % 10;
+  var sec1   = (sec - sec2) / 10;
+  document.getElementById('dig0').src = 'index_files/'+hour1+'.png';
+  document.getElementById('dig1').src = 'index_files/'+hour2+'.png';
+  document.getElementById('dig3').src = 'index_files/'+mine1+'.png';
+  document.getElementById('dig4').src = 'index_files/'+mine2+'.png';
+  document.getElementById('dig6').src = 'index_files/'+sec1+'.png';
+  document.getElementById('dig7').src = 'index_files/'+sec2+'.png';
+  DispWeekDays();
+  Blinks();
+  //setTimeout( moveDigital, 500 );
+}
+
+// Brink a colon.
+function Blinks(){
+  const COL_OBJ = document.getElementById('dig5').style;
+  if ( COL_OBJ.visibility=='hidden' )
+       COL_OBJ.visibility = 'visible';
+  else COL_OBJ.visibility = 'hidden';
+}
+setInterval( moveDigital,500 );
+//setInterval( Blinks,500 );
+
+// -----------------------------------------------------------------
+// Display date and day of the week.
+// -----------------------------------------------------------------
+function DispWeekDays(){
+  const monthEn = new Array(12);
+  monthEn[0]  = 'January';   // 1月
+  monthEn[1]  = 'February';  // 2月
+  monthEn[2]  = 'March';     // 3月
+  monthEn[3]  = 'April';     // 4月
+  monthEn[4]  = 'May';       // 5月
+  monthEn[5]  = 'June';      // 6月
+  monthEn[6]  = 'July';      // 7月
+  monthEn[7]  = 'August';    // 8月
+  monthEn[8]  = 'September'; // 9月
+  monthEn[9]  = 'October';   // 10月
+  monthEn[10] = 'November';  // 11月
+  monthEn[11] = 'December';  // 12月
+  const WeekEn = new Array(7);
+  WeekEn[0] = 'Sunday';
+  WeekEn[1] = 'Monday';
+  WeekEn[2] = 'Tuesday';
+  WeekEn[3] = 'Wednesday';
+  WeekEn[4] = 'Thursday';
+  WeekEn[5] = 'Friday';
+  WeekEn[6] = 'Saturday';
+  const WeekJn = new Array('日','月','火','水','木','金','土');
+
+  // Display of date.
+  var today  = new Date();
+  var Year = today.getFullYear();      //year
+  var Mon = monthEn[today.getMonth()]; //month
+  var Day = today.getDate();           //date
+  var Week = WeekEn[today.getDay()];   //day of the week
+  var nowdate = Year+' '+Mon+'-'+Day+' '+Week;
+  //日本語表記の場合は下記を使用
+  //var nowdate = Year+'年 ' +(today.getMonth()+1)+'月 '
+  //+Day+'日 ('+WeekJn[today.getDay()]+')';
+  document.getElementById('weekdays').innerHTML = nowdate;
+}
+}() ); //IIFE end
